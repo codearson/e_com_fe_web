44d0a354660b2bd3f7e0819d736bf6e84f767f1f
Rfs12 <rfsmarsh1234@gmail.com>
2025-08-15 23:45:12 +0530

    error solved in app.jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ import EditProduct from "./pages/EditProduct";
 import SellerProfile from "./pages/SellerProfile";
 import Cart from "./pages/Cart";
 import { CartProvider } from "./utils/CartContext.jsx";
-import SellerDashboard from "./pages/sellerDashBoard.jsx";
+import SellerDashboard from "./pages/sellerDashboard.jsx";
 
 function App() {
   return (
